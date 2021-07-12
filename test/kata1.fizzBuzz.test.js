63bc7f902250c2a67bb6f5d5505a3f4e627f1203
Erhan11 <erhan0@hotmail.com>
2021-07-13 00:52:56 +0300

    kata1 finish

--- test/kata1.fizzBuzz.test.js
+++ test/kata1.fizzBuzz.test.js
@@ -1,9 +1,15 @@
 const { fizzBuzz } = require("../src");
 
 describe("fizzBuzz", () => {
-  test("returns Fizz when passed a multiple of 3", () => {});
+  test("returns Fizz when passed a multiple of 3", () => {
+expect(fizzBuzz(3)).toBe('Fizz')
+  });
 
-  test("returns Buzz when passed a multiple of 5", () => {});
+  test("returns Buzz when passed a multiple of 5", () => {
+    expect(fizzBuzz(5)).toBe('Buzz')
+    expect(fizzBuzz(10)).toBe('Buzz')
+
+  });
 
   test("returns FizzBuzz when passed a multiple 3 and 5", () => {});
 
